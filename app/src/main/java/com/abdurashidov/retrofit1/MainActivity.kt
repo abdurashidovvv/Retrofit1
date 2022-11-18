70742894c1cc1438ab67a8bac03592cf43c0944a
Saydullo <abdurashidovsaydullo35@gmail.com>
2022-11-18 16:05:16 +0500

    AddTodo

--- app/src/main/java/com/abdurashidov/retrofit1/MainActivity.kt
+++ app/src/main/java/com/abdurashidov/retrofit1/MainActivity.kt
@@ -2,9 +2,14 @@ package com.abdurashidov.retrofit1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.abdurashidov.retrofit1.adapters.RvAdapter
 import com.abdurashidov.retrofit1.databinding.ActivityMainBinding
+import com.abdurashidov.retrofit1.databinding.DialogItemBinding
+import com.abdurashidov.retrofit1.models.AddTodoRequest
 import com.abdurashidov.retrofit1.models.Plan
 import com.abdurashidov.retrofit1.retrofit.ApiClient
 import retrofit2.Call
@@ -16,24 +21,79 @@ class MainActivity : AppCompatActivity(), RvAdapter.RvClick {
     private lateinit var rvAdapter: RvAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ApiClient.getRetrofitService().getAllTodo()
-            .enqueue(object : Callback<ArrayList<Plan>>{
+        getData()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.my_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        postData()
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getData() {
+        ApiClient.getRetrofitService().getAllTodo().enqueue(object : Callback<ArrayList<Plan>> {
                 override fun onResponse(
-                    call: Call<ArrayList<Plan>>,
-                    response: Response<ArrayList<Plan>>
+                    call: Call<ArrayList<Plan>>, response: Response<ArrayList<Plan>>
                 ) {
-                    rvAdapter=RvAdapter(response.body()!!, this@MainActivity)
-                    binding.myRv.adapter=rvAdapter
+                    rvAdapter = RvAdapter(response.body()!!, this@MainActivity)
+                    binding.myRv.adapter = rvAdapter
                 }
 
                 override fun onFailure(call: Call<ArrayList<Plan>>, t: Throwable) {
-                    Toast.makeText(this@MainActivity, "Internet bilan muammo bor.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this@MainActivity, "Internet bilan muammo bor.", Toast.LENGTH_SHORT
+                    ).show()
                 }
             })
+    }
 
+    private fun postData() {
+        val dialog = AlertDialog.Builder(this).create()
+        val dialogItemBinding = DialogItemBinding.inflate(layoutInflater)
+        dialog.setView(dialogItemBinding.root)
+
+        dialogItemBinding.apply {
+            save.setOnClickListener {
+                val addTodoRequest = AddTodoRequest(
+                    holat = status.selectedItem.toString(),
+                    matn = text.text.toString(),
+                    oxirgi_muddat = deadline.toString(),
+                    sarlavha = title.text.toString()
+                )
+                ApiClient.getRetrofitService().addTodo(addTodoRequest)
+                    .enqueue(object : Callback<Plan> {
+                        override fun onResponse(call: Call<Plan>, response: Response<Plan>) {
+                            if (response.isSuccessful) {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "${response.body()?.sarlavha} saqlandi!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                dialog.cancel()
+                                getData()
+                            }
+                        }
+
+                        override fun onFailure(call: Call<Plan>, t: Throwable) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Internet bilan bogliq muammo",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    })
+            }
+        }
+
+        dialog.show()
     }
 
     override fun onClick(label: Plan) {
